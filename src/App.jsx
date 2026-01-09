e545e05b00c192da572db000c7c0e832b965e7d1
ritam-g <ritammaty2006@gmail.com>
2026-01-09 23:11:55 +0530

    learning resposive ness uin tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import Section from './components/Section'
+import BelowSectionPart from './components/BelowSectionPart'
 
 function App() {
   return (
-    <div>
-      <h1>responsiv ness</h1>
+    <div className='app overflow-y-scroll overflow-x-hidden p-2 flex flex-col gap-2'>
+      <NavBar/>
+      <Section/>
+      <BelowSectionPart/>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css';
+import Image from './Context/Image.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App/>
+  <Image>
+    <App/>
+    </Image>
+  
 );
