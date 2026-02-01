563f40093adc62e8846840d82906b1734e4437a0
ritam-g <ritammaty2006@gmail.com>
2026-02-01 18:20:49 +0530

    redux practice

--- consept revision/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counterReducer";
-import themeReducer from "./features/theme";
-export const store=configureStore({
-    reducer:{
-        counter:counterReducer,
-        theme:themeReducer
-    }
-})
